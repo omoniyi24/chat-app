0e4057dc3341c0001bb5d66504dc865ea59ef605
Omoniyi Ilesanmi <omoniyi.ilesanmi@prunny.com>
2024-03-28 16:30:50 +0000

    implement rabbitmq

--- src/main/java/com/omoniyi24/chatapp/entity/ChatMessage.java
+++ src/main/java/com/omoniyi24/chatapp/entity/ChatMessage.java
@@ -16,37 +16,4 @@ public class ChatMessage {
     private String receiver;
     private String content;
 
-//    public ChatMessage() {
-//    }
-//
-//    @JsonCreator
-//    public ChatMessage(@JsonProperty("sender") String sender, @JsonProperty("receiver") String receiver, @JsonProperty("content") String content) {
-//        this.sender = sender;
-//        this.receiver = receiver;
-//        this.content = content;
-//    }
-
-//    public String getSender() {
-//        return sender;
-//    }
-//
-//    public void setSender(String sender) {
-//        this.sender = sender;
-//    }
-//
-//    public String getReceiver() {
-//        return receiver;
-//    }
-//
-//    public void setReceiver(String reciever) {
-//        this.receiver = reciever;
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
 }
