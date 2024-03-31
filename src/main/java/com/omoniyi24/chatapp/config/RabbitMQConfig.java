e47971b0a93f61ef5ec7242e222c056eb310c252
Omoniyi Ilesanmi <omoniyi.ilesanmi@prunny.com>
2024-03-31 13:05:00 +0100

    dockerize the application

--- src/main/java/com/omoniyi24/chatapp/config/RabbitMQConfig.java
+++ src/main/java/com/omoniyi24/chatapp/config/RabbitMQConfig.java
@@ -1,58 +1,69 @@
 package com.omoniyi24.chatapp.config;
 
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class RabbitMQConfig {
 
-    @Bean
-    Queue queue() {
-        return new Queue("${chat.rabbitmq.queue}", false);
-    }
+    @Value("${spring.rabbitmq.host}")
+    private String SPRING_RABBITMQ_HOST;
+
+    @Value("${spring.rabbitmq.port}")
+    private int SPRING_RABBITMQ_PORT;
+
+    @Value("${spring.rabbitmq.username}")
+    private String SPRING_RABBITMQ_USERNAME;
+
+    @Value("${spring.rabbitmq.password}")
+    private String SPRING_RABBITMQ_PASSWORD;
+
+    @Value("${chat.rabbitmq.queue}")
+    private String SPRING_RABBITMQ_QUEUE;
+
+    @Value("${chat.rabbitmq.exchange}")
+    private String SPRING_RABBITMQ_EXCHANGE;
+
+    @Value("${chat.rabbitmq.routingkey}")
+    private String SPRING_RABBITMQ_KEY;
 
     @Bean
-    Exchange exchange() {
-        return new TopicExchange("${chat.rabbitmq.exchange}");
-    }
-
-    @Bean
-    Binding binding(Queue queue, Exchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("${chat.rabbitmq.routingkey}").noargs();
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    public RabbitTemplate rabbitTemplate(){
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(SPRING_RABBITMQ_HOST,SPRING_RABBITMQ_PORT);
+        connectionFactory.setUsername(SPRING_RABBITMQ_USERNAME);
+        connectionFactory.setPassword(SPRING_RABBITMQ_PASSWORD);
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setExchange(SPRING_RABBITMQ_EXCHANGE);
+        rabbitTemplate.setRoutingKey(SPRING_RABBITMQ_KEY);
         rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
         return rabbitTemplate;
     }
 
     @Bean
-    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    DirectExchange exchange() {
+        return new DirectExchange(SPRING_RABBITMQ_EXCHANGE, true, false);
     }
 
     @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory,
-                                                                               MessageConverter jsonMessageConverter) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(jsonMessageConverter);
-        return factory;
+    public Queue queue() {
+        return new Queue(SPRING_RABBITMQ_QUEUE, true);
+    }
+
+    @Bean
+    Binding exchangeBinding(DirectExchange exchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(exchange).with(SPRING_RABBITMQ_KEY);
+    }
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
